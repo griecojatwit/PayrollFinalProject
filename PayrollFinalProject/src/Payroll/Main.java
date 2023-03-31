18281dd253a5ba51e717cc4824d22d837d56bafa
tkupf <tkupf@LAPTOP-I1RNVKA0.wit.private>
2023-03-31 13:23:06 -0400

    Added junk into main

--- PayrollFinalProject/src/Payroll/Main.java
+++ PayrollFinalProject/src/Payroll/Main.java
@@ -1,9 +1,32 @@
 package Payroll;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Add button to add new employee
+		System.out.println("Enter employees first name: ");
+		Scanner scanner = new Scanner(System.in);
+		String firstName = scanner.nextLine();
+		
+		System.out.println("Enter employees last name: ");
+		String lastName = scanner.nextLine();
+		
+		System.out.println("Enter employees job title: ");
+		String jobTitle = scanner.nextLine();
+		
+		System.out.println("Enter employees salary: ");
+		double salary = scanner.nextDouble();
+		
+		scanner.close();
+		
+		System.out.println(firstName + " " + lastName + " " + jobTitle + " " + salary + ".");
+		
+		
+		
+		
 
 	}
 
