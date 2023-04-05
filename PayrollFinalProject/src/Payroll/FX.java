9b7c92fd693be0e30adfeae252f1fa25e58527d0
John Grieco <John Grieco@DESKTOP-PFOULAB.wit.private>
2023-04-04 21:18:11 -0400

    FX code

--- PayrollFinalProject/src/Payroll/FX.java
+++ PayrollFinalProject/src/Payroll/FX.java
@@ -1,5 +1,19 @@
 package Payroll;
 
-public class FX {
+import javafx.application.*;
+import javafx.stage.*;
 
+public class FX extends Application {
+@Override
+	
+public void start (Stage s) {
+		s.setWidth(800);
+		s.setHeight(600);
+		s.show();
+	}
+
+	public static void main(String[] args ) {
+		launch(args);
+	}
 }
+
--- PayrollFinalProject/src/Payroll/module-info.java
+++ PayrollFinalProject/src/Payroll/module-info.java
@@ -1,2 +1,11 @@
 module PayrollFinalProject {
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.swt;
+	requires javafx.web;
+	exports Payroll;
 }
